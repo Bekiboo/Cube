618cb54a5ca57de3dcd5f498a484f7d79e14505e
Bekiboo <julien.connault@gmail.com>
2024-09-25 14:57:18 +0300

    Fix Player collider, ground, and many other stuff

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { T, useTask } from '@threlte/core';
-	import { Gizmo, OrbitControls, HTML } from '@threlte/extras';
-	import { DEG2RAD } from 'three/src/math/MathUtils.js';
-	import { handleKeysDown, handleKeysUp } from './keyboardInputHandler';
-	import { generatePyramid } from './pyramidGenerator';
+	import { T, useTask } from '@threlte/core'
+	import { Gizmo, OrbitControls, HTML } from '@threlte/extras'
+	import { DEG2RAD } from 'three/src/math/MathUtils.js'
+	import { handleKeysDown, handleKeysUp } from './keyboardInputHandler'
+	import { generatePyramid } from './pyramidGenerator'
 	import {
 		AutoColliders,
 		Collider,
@@ -11,49 +11,69 @@
 		BasicPlayerController,
 		Attractor,
 		Debug
-	} from '@threlte/rapier';
-	import { MeshStandardMaterial, CylinderGeometry } from 'three';
-	import { onMount } from 'svelte';
+	} from '@threlte/rapier'
+	import { MeshStandardMaterial, CylinderGeometry, Vector3, Vector4 } from 'three'
 
-	import { blur } from 'svelte/transition';
+	import { Collider as C } from '@dimforge/rapier3d-compat'
+	import Menu from './Menu.svelte'
 
-	let playerPos = [-5, 2, -5];
+	let playerPos = [-5, 2, -5]
 
-	let keysPressed = [];
+	let keysPressed = []
 
-	let strength = 0.125;
+	let strength = 0.0625
 
-	const handleKeyDown = (e) => (keysPressed = handleKeysDown(e, keysPressed));
-	const handleKeyUp = (e) => (keysPressed = handleKeysUp(e, keysPressed));
+	const handleKeyDown = (e) => (keysPressed = handleKeysDown(e, keysPressed))
+	const handleKeyUp = (e) => (keysPressed = handleKeysUp(e, keysPressed))
 
 	let player = {
+		size: [0.5, 0.5, 0.5],
 		pos: { x: -5, y: 0, z: -5 }
-	};
+	}
 
-	let playerCollider;
-	onMount(() => {});
-	console.log('🚀 ~ playerCollider:', playerCollider);
+	let collider
+
+	let ground = {
+		width: 35,
+		depth: 35
+	}
 
 	useTask((delta) => {
-		// if (keysPressed.includes('forward')) playerCollider.setTranslation({}) -= 8 * delta;
-		// if (keysPressed.includes('backward')) playerCollider.setTranslation({}) += 8 * delta;
-		// if (keysPressed.includes('left')) playerCollider.setTranslation({}) -= 8 * delta;
-		// if (keysPressed.includes('right')) playerCollider.setTranslation({}) += 8 * delta;
-		if (keysPressed.includes('forward')) player.pos.z -= 8 * delta;
-		if (keysPressed.includes('backward')) player.pos.z += 8 * delta;
-		if (keysPressed.includes('left')) player.pos.x -= 8 * delta;
-		if (keysPressed.includes('right')) player.pos.x += 8 * delta;
+		if (keysPressed.includes('forward')) player.pos.z -= 10 * delta
+		if (keysPressed.includes('backward')) player.pos.z += 10 * delta
+		if (keysPressed.includes('left')) player.pos.x -= 10 * delta
+		if (keysPressed.includes('right')) player.pos.x += 10 * delta
 
-		player = player;
-	});
+		switch (true) {
+			case player.pos.x > ground.width / 2 - player.size[0]:
+				player.pos.x = ground.width / 2 - player.size[0]
+				break
+			case player.pos.x < -ground.width / 2 + player.size[0]:
+				player.pos.x = -ground.width / 2 + player.size[0]
+				break
+			case player.pos.z > ground.depth / 2 - player.size[2]:
+				player.pos.z = ground.depth / 2 - player.size[2]
+				break
+			case player.pos.z < -ground.depth / 2 + player.size[2]:
+				player.pos.z = -ground.depth / 2 + player.size[2]
+				break
+		}
 
-	const pyramid = generatePyramid(1, 0.5, 0.5);
+		player = player
+	})
 
-	let open = true;
+	const pyramid = generatePyramid(1, 0.3, 0.3)
+
+	let open = true
+
+	$: collider?.setTranslation(new Vector3(player.pos.x, player.pos.y, player.pos.z))
+	$: collider?.setRotation(new Vector4(1))
 </script>
 
 <!-- <T.PerspectiveCamera position={cameraPos} makeDefault fov={5} near={0.1} far={1000000}>
 	</T.PerspectiveCamera> -->
+
+<!-- <Debug /> -->
 
 <T.PerspectiveCamera
 	makeDefault
@@ -63,7 +83,7 @@
 	near={0.1}
 	far={1000000}
 	on:create={({ ref }) => {
-		ref.lookAt(0, 1, 0);
+		ref.lookAt(0, 1, 0)
 	}}
 >
 	<OrbitControls maxPolarAngle={85 * DEG2RAD} minPolarAngle={20 * DEG2RAD} />
@@ -80,51 +100,24 @@
 <T.AmbientLight intensity={0.5} />
 
 <!-- PLAYER -->
-<!-- <BasicPlayerController
-	position={playerPos}
-	speed={8}
-	radius={0.3}
-	height={1.8}
-	jumpStrength={4}
-	groundCollisionGroups={[15]}
-	playerCollisionGroups={[0]}
->
-	<T.Mesh
-		position.y={0.9}
-		receiveShadow
-		castShadow
-		geometry={new CylinderGeometry(0.3, 0.3, 1.8, 6)}
-		material={new MeshStandardMaterial()}
-	/>
-	<Attractor range={30} {strength} />
-</BasicPlayerController> -->
-
 <RigidBody type="dynamic">
 	<T.Mesh position={[player.pos.x, player.pos.y, player.pos.z]} castShadow>
-		<Collider
-			shape={'cuboid'}
-			args={[1, 1, 1]}
-			contactForceEventThreshold={30}
-			restitution={0.4}
-			friction={1}
-		/>
 		<T.BoxGeometry args={[1, 1, 1]} />
-		<T.MeshStandardMaterial color={'red'} />
-		<!-- <Attractor range={200} strength={0.125} /> -->
+		<T.MeshStandardMaterial color={'#fab900'} />
+		<Attractor range={30} {strength} />
 	</T.Mesh>
 </RigidBody>
 
 <Collider
 	shape={'cuboid'}
-	args={[1, 1, 1]}
+	args={[0.5, 0.5, 0.5]}
 	contactForceEventThreshold={30}
 	restitution={0.4}
 	friction={1}
+	bind:collider
 />
 
-<Debug />
-
-<!-- TEST -->
+<!-- PYRAMID -->
 {#each pyramid.stones as [x, y, z], i}
 	<T.Group position={[x, y, z]}>
 		<RigidBody type="dynamic">
@@ -145,31 +138,13 @@
 	</T.Group>
 {/each}
 
-<HTML transform position.y={3} pointerEvents={'none'}>
-	{#key open}
-		<small
-			in:blur={{
-				amount: 15,
-				duration: 300
-			}}
-			out:blur={{
-				amount: 15,
-				duration: 300
-			}}
-			class="door"
-			class:closed={!open}
-			class:open
-		>
-			{open ? 'UNLOCKED' : 'LOCKED'}
-		</small>
-	{/key}
-</HTML>
+<Menu />
 
 <!-- GROUND -->
 <T.Group>
 	<AutoColliders shape={'cuboid'} friction={0.4}>
 		<T.Mesh position={[0, -1, 0]} receiveShadow>
-			<T.BoxGeometry args={[40, 1, 40]} />
+			<T.BoxGeometry args={[ground.width, 1, ground.depth]} />
 			<T.MeshStandardMaterial color="#777" />
 		</T.Mesh>
 	</AutoColliders>
@@ -178,20 +153,20 @@
 <!-- WALLS -->
 <T.Group>
 	<AutoColliders shape={'cuboid'} friction={0}>
-		<T.Mesh position={[0, 0, -20]} receiveShadow>
-			<T.BoxGeometry args={[41, 2, 1]} />
+		<T.Mesh position={[0, 0, -(ground.width + 1) / 2]} receiveShadow>
+			<T.BoxGeometry args={[ground.width + 2, 2, 1]} />
 			<T.MeshStandardMaterial color="#888" />
 		</T.Mesh>
-		<T.Mesh position={[0, 0, 20]} receiveShadow>
-			<T.BoxGeometry args={[41, 2, 1]} />
+		<T.Mesh position={[0, 0, (ground.width + 1) / 2]} receiveShadow>
+			<T.BoxGeometry args={[ground.width + 2, 2, 1]} />
 			<T.MeshStandardMaterial color="#888" />
 		</T.Mesh>
-		<T.Mesh position={[-20, 0, 0]} receiveShadow>
-			<T.BoxGeometry args={[1, 2, 41]} />
+		<T.Mesh position={[-(ground.width + 1) / 2, 0, 0]} receiveShadow>
+			<T.BoxGeometry args={[1, 2, ground.width + 2]} />
 			<T.MeshStandardMaterial color="#888" />
 		</T.Mesh>
-		<T.Mesh position={[20, 0, 0]} receiveShadow>
-			<T.BoxGeometry args={[1, 2, 41]} />
+		<T.Mesh position={[(ground.width + 1) / 2, 0, 0]} receiveShadow>
+			<T.BoxGeometry args={[1, 2, ground.width + 2]} />
 			<T.MeshStandardMaterial color="#888" />
 		</T.Mesh>
 	</AutoColliders>
@@ -202,29 +177,6 @@
 	<Collider sensor shape={'cuboid'} args={[1, 1, 1]} />
 </RigidBody> -->
 
-<Gizmo horizontalPlacement="left" paddingX={20} paddingY={20} />
+<!-- <Gizmo horizontalPlacement="left" paddingX={20} paddingY={20} /> -->
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
-
-<style>
-	.door {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		font-size: 2rem;
-		color: white;
-		background: black;
-		padding: 0.5rem 1rem;
-		border-radius: 0.5rem;
-		transition: 0.5s;
-	}
-
-	.door.closed {
-		background: red;
-	}
-
-	.door.open {
-		background: green;
-	}
-</style>
