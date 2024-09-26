c22b9a7f1f79cbcd1cca2929b26d9b98ec124228
Bekiboo <julien.connault@gmail.com>
2024-09-26 09:56:50 +0300

    Refactor Menu.svelte component

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -1,31 +1,72 @@
 <script lang="ts">
-	import { player } from '$lib/stores'
+	import { game, player } from '$lib/stores'
+	import { spring } from 'svelte/motion'
 	import { HTML } from '@threlte/extras'
-	import { blur } from 'svelte/transition'
+	import { fade, fly } from 'svelte/transition'
+	import { quintOut } from 'svelte/easing'
+
+	let isPointerDown = false
+
+	let htmlPosZ = -17.5
+	// let htmlPosZ = spring(0)
+	// $: htmlPosZ.set(isPointerDown ? -0.15 : 0, {
+	// 	hard: isPointerDown
+	// })
 </script>
 
-<HTML transform position.y={3} position.z={-17.5} pointerEvents={'none'}>
-	<!-- HP -->
-	<div class="w-96 h-16 bg-black duration-150 border-white border-8">
-		<div class="bg-red-500 h-full" style="width: {(100 / $player.maxHP) * $player.HP}%;"></div>
-	</div>
-	<!-- {#key open}
-		<small
-			in:blur={{
-				amount: 15,
-				duration: 300
-			}}
-			out:blur={{
-				amount: 15,
-				duration: 300
-			}}
-			class="door"
-			class:closed={!open}
-			class:open
+<HTML transform position.y={3} position.z={htmlPosZ} class="bg-red-500">
+	{#if $game.state === 'Menu'}
+		<div class="flex flex-col" in:fade={{ duration: 500, delay: 500, easing: quintOut }}>
+			<h1 class="font-black text-red-500 text-9xl">CUBE</h1>
+			<button
+				class="bg-blue-500 text-4xl mx-auto hover:bg-blue-400 text-white p-2 mt-4"
+				on:pointerdown={() => (isPointerDown = true)}
+				on:pointerup={() => (isPointerDown = false)}
+				on:pointercancel={() => {
+					isPointerDown = false
+				}}
+				on:click={() => $game.startGame()}>Start</button
+			>
+		</div>
+	{/if}
+	{#if $game.state === 'GameOver'}
+		<div
+			class="flex justify-center items-center w-screen h-screen"
+			in:fade={{ duration: 500, delay: 500, easing: quintOut }}
 		>
-			{open ? 'UNLOCKED' : 'LOCKED'}
-		</small>
-	{/key} -->
+			<div class="bg-white p-4 rounded-lg">
+				<h1 class="text-2xl">Game Over</h1>
+				<button
+					class="bg-blue-500 text-white p-2 rounded-lg mt-4"
+					on:click={() => $game.startGame()}>Restart</button
+				>
+			</div>
+		</div>
+	{/if}
+
+	{#if $game.state === 'Playing'}
+		<!-- <div class="bg-red-500 w-screen">
+                <div class="text-white left-2 top-2">
+                    <div class="flex justify-center w-full">
+                        <kbd class="kbd">W</kbd>
+                    </div>
+                    <div class="flex justify-center w-full">
+                        <kbd class="kbd">A</kbd>
+                        <kbd class="kbd">S</kbd>
+                        <kbd class="kbd">D</kbd>
+                    </div>
+                    <div class="flex justify-center w-full mt-2">
+                        <kbd class="kbd">Space</kbd>
+                    </div>
+                </div>
+            </div> -->
+		<div
+			in:fade={{ duration: 500, delay: 500, easing: quintOut }}
+			class="w-96 h-16 bg-black duration-150 border-white border-8"
+		>
+			<div class="bg-red-500 h-full" style="width: {(100 / $player.maxHP) * $player.HP}%;"></div>
+		</div>
+	{/if}
 </HTML>
 
 <style>
